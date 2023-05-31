656b3655db34ddd4d18acad972181dbd6796f2ee
Brian Nguyen <briannguyen.sd@gmail.com>
2023-05-31 18:32:26 -0400

    Add flickering title - Swap card image on click

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,17 @@ import Card7 from "./assets/cards/7.jpg";
 import Card8 from "./assets/cards/8.jpg";
 import Card9 from "./assets/cards/9.jpg";
 import Card10 from "./assets/cards/10.jpg";
+import Alt0 from "./assets/cards-alt/0.jpg";
+import Alt1 from "./assets/cards-alt/1.jpg";
+import Alt2 from "./assets/cards-alt/2.jpg";
+import Alt3 from "./assets/cards-alt/3.jpg";
+import Alt4 from "./assets/cards-alt/4.jpg";
+import Alt5 from "./assets/cards-alt/5.jpg";
+import Alt6 from "./assets/cards-alt/6.jpg";
+import Alt7 from "./assets/cards-alt/7.jpg";
+import Alt8 from "./assets/cards-alt/8.jpg";
+import Alt9 from "./assets/cards-alt/9.jpg";
+import Alt10 from "./assets/cards-alt/10.jpg";
 
 function App() {
   const sandbox = useRef<HTMLDivElement>(null);
@@ -23,6 +34,11 @@ function App() {
       if (sandbox.current === null) {
         return;
       }
+      // Ignore if click is on a card
+      if ((e.target as HTMLElement).closest(".card") !== null) {
+        return;
+      }
+
       // Save initial position of mouse
       sandbox.current.dataset.initialx = e.clientX.toString();
       sandbox.current.dataset.initialy = e.clientY.toString();
@@ -256,17 +272,105 @@ function App() {
         data-zoom="0"
         ref={sandbox}
       >
-        <Card name="sangyeon" src={Card0} id={0} drag={isDragging} />
-        <Card name="jacob" src={Card1} id={1} drag={isDragging} />
-        <Card name="younghoon" src={Card2} id={2} drag={isDragging} />
-        <Card name="hyunjae" src={Card3} id={3} drag={isDragging} />
-        <Card name="juyeon" src={Card4} id={4} drag={isDragging} />
-        <Card name="kevin" src={Card5} id={5} drag={isDragging} />
-        <Card name="chanhee" src={Card6} id={6} drag={isDragging} />
-        <Card name="changmin" src={Card7} id={7} drag={isDragging} />
-        <Card name="haknyeon" src={Card8} id={8} drag={isDragging} />
-        <Card name="sunwoo" src={Card9} id={9} drag={isDragging} />
-        <Card name="eric" src={Card10} id={10} drag={isDragging} />
+        <Card
+          name="SANGYEON"
+          position="MAIN VOCALIST"
+          year={"96"}
+          src={Card0}
+          altSrc={Alt0}
+          id={0}
+          drag={isDragging}
+        />
+        <Card
+          name="JACOB"
+          position="LEAD RAPPER"
+          year={"97"}
+          src={Card1}
+          altSrc={Alt1}
+          id={1}
+          drag={isDragging}
+        />
+        <Card
+          name="YOUNGHOON"
+          position="VISUAL"
+          year={"97"}
+          src={Card2}
+          altSrc={Alt2}
+          id={2}
+          drag={isDragging}
+        />
+        <Card
+          name="HYUNJAE"
+          position="VISUAL"
+          year={"97"}
+          src={Card3}
+          altSrc={Alt3}
+          id={3}
+          drag={isDragging}
+        />
+        <Card
+          name="JUYEON"
+          position="MAIN DANCER"
+          year={"98"}
+          src={Card4}
+          altSrc={Alt4}
+          id={4}
+          drag={isDragging}
+        />
+        <Card
+          name="KEVIN"
+          position="MAIN VOCALIST"
+          year={"98"}
+          src={Card5}
+          altSrc={Alt5}
+          id={5}
+          drag={isDragging}
+        />
+        <Card
+          name="CHANHEE"
+          position="MAIN VOCALIST"
+          year={"98"}
+          src={Card6}
+          altSrc={Alt6}
+          id={6}
+          drag={isDragging}
+        />
+        <Card
+          name="CHANGMIN"
+          position="MAIN DANCER"
+          year={"98"}
+          src={Card7}
+          altSrc={Alt7}
+          id={7}
+          drag={isDragging}
+        />
+        <Card
+          name="HAKNYEON"
+          position="LEAD DANCER"
+          year={"99"}
+          src={Card8}
+          altSrc={Alt8}
+          id={8}
+          drag={isDragging}
+        />
+        <Card
+          name="SUNWOO"
+          position="MAIN RAPPER"
+          year={"00"}
+          src={Card9}
+          altSrc={Alt9}
+          id={9}
+          drag={isDragging}
+        />
+        <Card
+          name="ERIC"
+          position="LEAD RAPPER"
+          year={"00"}
+          src={Card10}
+          altSrc={Alt10}
+          id={10}
+          drag={isDragging}
+        />
       </div>
       <div id="interface">
         <div id="zoom" className="interface-content">
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,26 @@
-import { FC, useRef, useEffect } from "react";
+import { FC, useRef, useEffect, useState } from "react";
 import { gsap } from "gsap";
+import { MdSwapVert } from "react-icons/md";
 
 interface CardProps {
   name: string;
+  position: string;
+  year: string;
   src: string;
+  altSrc: string;
   id: number;
   drag: boolean;
 }
 
-const Card: FC<CardProps> = ({ name, src, id, drag }) => {
+const Card: FC<CardProps> = ({
+  name,
+  position,
+  year,
+  src,
+  altSrc,
+  id,
+  drag,
+}) => {
   const imageRef = useRef<HTMLImageElement>(null);
   useEffect(() => {
     if (imageRef.current) {
@@ -19,6 +31,7 @@ const Card: FC<CardProps> = ({ name, src, id, drag }) => {
   // Hover effect
   const grainRef = useRef<SVGFETurbulenceElement>(null);
   const mapRef = useRef<SVGFEDisplacementMapElement>(null);
+  const titleRef = useRef<HTMLDivElement>(null);
   const playDuration = 0.5;
   const cardHover = () => {
     if (drag) {
@@ -29,6 +42,10 @@ const Card: FC<CardProps> = ({ name, src, id, drag }) => {
     }
     if (mapRef.current === null) {
       return;
+    }
+
+    if (titleRef.current) {
+      titleRef.current.classList.add("active");
     }
 
     // Animate grain by incrementing seed values
@@ -64,23 +81,78 @@ const Card: FC<CardProps> = ({ name, src, id, drag }) => {
     });
   };
 
+  const [isAlt, setIsAlt] = useState(false);
+  useEffect(() => {
+    if (imageRef.current === null) {
+      return;
+    }
+
+    if (isAlt) {
+      imageRef.current.src = altSrc;
+    } else {
+      imageRef.current.src = src;
+    }
+  }, [isAlt]);
+
+  const constructText = (text: string) => {
+    const spans = [];
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] === " ") {
+        spans.push(<span key={i}>&nbsp;</span>);
+      } else {
+        spans.push(
+          <span
+            key={i}
+            className="title-char"
+            style={{ ["--delay" as string]: `${Math.random() * -20}s` }}
+          >
+            {text[i]}
+          </span>
+        );
+      }
+    }
+    return spans;
+  };
+
   return (
-    <div className="card" id={`card-${id}`} onMouseEnter={cardHover}>
-      <img src={src} draggable={false} ref={imageRef} />
-      <svg>
-        <defs>
-          <filter id={`grain-${id}`}>
-            <feTurbulence
-              baseFrequency="0.8, 0.8"
-              seed={0}
-              type="fractalNoise"
-              result="noise"
-              ref={grainRef}
-            ></feTurbulence>
-            <feDisplacementMap in="SourceGraphic" in2="noise" ref={mapRef} />
-          </filter>
-        </defs>
-      </svg>
+    <div className="card-container">
+      <div
+        className="card"
+        id={`card-${id}`}
+        onMouseEnter={cardHover}
+        onMouseLeave={() => {
+          if (titleRef.current) {
+            titleRef.current.classList.remove("active");
+          }
+        }}
+        onClick={() => {
+          setIsAlt(!isAlt);
+        }}
+      >
+        <img src={src} draggable={false} ref={imageRef} />
+        <svg>
+          <defs>
+            <filter id={`grain-${id}`}>
+              <feTurbulence
+                baseFrequency="0.8, 0.8"
+                seed={0}
+                type="fractalNoise"
+                result="noise"
+                ref={grainRef}
+              ></feTurbulence>
+              <feDisplacementMap in="SourceGraphic" in2="noise" ref={mapRef} />
+            </filter>
+          </defs>
+        </svg>
+      </div>
+      <MdSwapVert color="white" size={30} className="swap" />
+      <div className="title" ref={titleRef}>
+        <div>
+          <span className="name">{constructText(name)}</span>
+          <span className="year">{constructText(`'${year}`)}</span>
+        </div>
+        <span className="position">{constructText(position)}</span>
+      </div>
     </div>
   );
 };
