b5b12f9aa8556c165696391a282c900970c67ad9
Brian Nguyen <briannguyen.sd@gmail.com>
2023-06-02 14:42:17 -0400

    Add responsive mobile hero section

--- src/components/Mobile.tsx
+++ src/components/Mobile.tsx
@@ -1,8 +1,24 @@
 import Cards from "./Cards";
+import Logo from "./Logo";
+import { HiChevronDoubleDown } from "react-icons/hi";
 
 const Mobile = () => {
   return (
-    <div className="scrollable">
+    <div id="grid-mobile">
+      <section id="hero-mobile">
+        <Logo />
+        <a
+          id="teaser"
+          href="https://www.youtube.com/watch?v=5bGIWUAq1SY"
+          target="_blank"
+        >
+          WATCH TEASER &#x2197;
+        </a>
+        <div id="hero-prompt">
+          <span>CONCEPT PHOTOS </span>
+          <HiChevronDoubleDown size={20} color="white" />
+        </div>
+      </section>
       <Cards isDragging={false} />
     </div>
   );
